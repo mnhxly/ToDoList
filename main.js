a690dfc2b60dd57bb28f3950251d36fafa979b08
Malte <malte.hoenicke@gmail.com>
2018-03-26 08:12:27 +0200

    update

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
   var liCounter = 0;
 
   function saveToDo() {
-    var tableRef = document.getElementById('tabelToDo');
+    var tableBodyRef = document.getElementById('tableBody');
     var task = document.getElementById('newTask').value;
     var category = document.getElementById('todoCategory').value;
     var date = document.getElementById('dateChooser').value;
     if (task && category != 0 && date) {
 
       // Insert a row in the table at the last row
-      var newRow   = tableRef.insertRow(tableRef.rows.length);
-      // Insert a cell in the row at index 0
-      var newCellName  = newRow.insertCell(0);
-      var newCellCategory  = newRow.insertCell(1);
-      var newCellDate  = newRow.insertCell(2);
-      var newCellDelete  = newRow.insertCell(3);
+      var newRow = tableBodyRef.insertRow(0);
 
+      // Insert a cell in the row at index 0
+      var newCellName = newRow.insertCell(0);
+      var newCellCategory = newRow.insertCell(1);
+      var newCellDate = newRow.insertCell(2);
+      var newCellDelete = newRow.insertCell(3);
+
+      //Insert the Fiels Values into the Table Cells
       newCellName.appendChild(document.createTextNode(task));
       newCellCategory.appendChild(document.createTextNode(category));
       newCellDate.appendChild(document.createTextNode(date));
-      newCellDelete.appendChild(document.createButton(createCheckButton()));
-      var removeButton = createButton();
+      newCellDelete.appendChild(createCheckButton());
+
+      //Clear all the Input Fields
       document.getElementById("newTask").value = "";
       document.getElementById("todoCategory").value = "";
       document.getElementById("dateChooser").value = "";
@@ -28,10 +31,19 @@
     }
   }
 
+  function checkItem() {
+    var tRow = this.parentNode.parentNode;
+    var parent = tRow.parentNode;
+
+    console.log("checked");
+  }
+
   function createCheckButton() {
-    var check = document.createElement('input');
-    check.setAttribute("type", "image");
-    check.setAttribute("id", "checkImage");
-    check.setAttribute("src", "icon_check.png");
-    return remove;
+    var btn = document.createElement('input');
+    btn.type = "image";
+    btn.className = "btn";
+    btn.src = "CheckIcon.png";
+    btn.onclick = checkItem();
+
+    return btn;
   }
