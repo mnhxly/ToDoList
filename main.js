4a8669e402c7756b662123843d6435e0377ef4f1
Malte <malte.hoenicke@gmail.com>
2018-04-03 21:23:07 +0200

    merge

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
   var liCounter = 0;
+  localStorage.task =
 
-  function saveToDo() {
-    var tableBodyRef = document.getElementById('tableBody');
-    var task = document.getElementById('newTask').value;
-    var category = document.getElementById('todoCategory').value;
-    var date = document.getElementById('dateChooser').value;
-    if (task && category != 0 && date) {
+    function saveToDo() {
+      var tableBodyRef = document.getElementById('tableBody');
+      var task = document.getElementById('newTask').value;
+      var category = document.getElementById('todoCategory').value;
+      var date = document.getElementById('dateChooser').value;
+      if (task && category != 0) {
 
-      // Insert a row in the table at the last row
-      var newRow = tableBodyRef.insertRow(0);
+        // Insert a row in the table at the last row
+        var newRow = tableBodyRef.insertRow(0);
 
-      // Insert a cell in the row at index 0
-      var newCellName = newRow.insertCell(0);
-      var newCellCategory = newRow.insertCell(1);
-      var newCellDate = newRow.insertCell(2);
-      var newCellDelete = newRow.insertCell(3);
+        // Insert a cell in the row at index 0
+        var newCellName = newRow.insertCell(0);
+        var newCellCategory = newRow.insertCell(1);
+        var newCellDate = newRow.insertCell(2);
+        var newCellDelete = newRow.insertCell(3);
 
-      //Insert the Fiels Values into the Table Cells
-      newCellName.appendChild(document.createTextNode(task));
-      newCellCategory.appendChild(document.createTextNode(category));
-      newCellDate.appendChild(document.createTextNode(date));
-      newCellDelete.appendChild(createCheckButton());
+        //Insert the Fiels Values into the Table Cells
+        newCellName.appendChild(document.createTextNode(task));
+        newCellCategory.appendChild(document.createTextNode(category));
+        newCellDate.appendChild(document.createTextNode(date));
+        newCellDelete.appendChild(createCheckButton());
 
-      //Clear all the Input Fields
-      document.getElementById("newTask").value = "";
-      document.getElementById("todoCategory").value = "";
-      document.getElementById("dateChooser").value = "";
-    } else {
-      alert("Please fill out everything!")
+        //Clear all the Input Fields
+        document.getElementById("newTask").value = "";
+        document.getElementById("todoCategory").value = "";
+        document.getElementById("dateChooser").value = "";
+      } else {
+        alert("Please fill out everything!")
+      }
     }
-  }
 
   function checkItem() {
     var tRow = this.parentNode.parentNode;
